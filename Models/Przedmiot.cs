9f8a60fce3eaaf91b83ca186f6dd6ba3fc35cdfe
Lukasz0411 <lukaszmejster44@gmail.com>
2025-03-12 09:18:46 +0100

    adding commit Przedmiot

---
+++ Models/Przedmiot.cs
@@ -0,0 +1,13 @@
+﻿using static Aplikacja_tworzeniebazy.Models.Modele_klas;
+
+namespace MVC_EduScanner.Models
+{
+    public class Przedmiot
+    {
+        public int PrzedmiotID { get; set; }
+        public string NazwaPrzedmiotu { get; set; } = string.Empty;
+
+        // Relacja wiele-do-wielu z prowadzącymi
+        public List<PrzedmiotProwadzacy> PrzedmiotProwadzacy { get; set; } = new List<PrzedmiotProwadzacy>();
+    }
+}
